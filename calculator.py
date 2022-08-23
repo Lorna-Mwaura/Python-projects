64e6b1cb8caeb9b4fb2a97c7d4e88602c198e17d
@Andy <joshua.mulonga@student.moringaschool.com>
2022-08-23 08:46:04 +0300

    ft:add math operators

--- calculator.py
+++ calculator.py
@@ -1,1 +1,25 @@
 # calculator application
+print("welcome to our calculator")
+print('*'*40)
+print('''Choose operation
+1.Addition
+2.Subtraction
+3.Multiplication
+4.Division
+''')
+
+operation =int(input())
+if operation <=4:
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+
+    if operation == 1:
+        print(float(num1) + float(num2))
+    elif operation == 2:
+        print(float(num1) - float(num2))
+    elif operation == 3:
+        print(float(num1) * float(num2))
+    elif operation == 4:
+        print(float(num1) / float(num2))
+else:
+    print("Choose valid operation")
